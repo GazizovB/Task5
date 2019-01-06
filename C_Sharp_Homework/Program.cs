db7fdbbd558beaed0099439eb9f905897754e7f5
Бауыржан <@->
2019-01-07 01:27:48 +0600

    Добавьте файлы проекта.

---
+++ C_Sharp_Homework/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Number(number);
+
+            Console.ReadLine();
+        }
+
+        public static int Number(int number)
+        {
+            Console.Write("Результат: ");
+            while (number > 0)
+            {
+                int number1 = number % 10;
+                Console.Write(number1);
+                number /= 10;
+            }
+            return number;
+        }
+    }
+}
